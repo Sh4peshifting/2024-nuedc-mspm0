0ff30c1def5fa1243f640a0152adae6c7b842053
Sh4peshifting <673656911@qq.com>
2024-08-02 16:52:45 +0800

    chore: change button pins

--- mspm0-proj/screen/disp.c
+++ mspm0-proj/screen/disp.c
@@ -4,9 +4,10 @@ extern float volt_rms;
 extern float curr_rms;
 extern float AP,PF;
 extern float curr_thd;
+extern float harmonic[16];
 
 float volt_disp, curr_disp, power_disp, pf_disp, thd_disp;
-uint8_t page;
+uint8_t page, harm_page;
 
 void disp_param_calib()
 {
@@ -30,6 +31,31 @@ void disp_printf(uint8_t disp_row, uint8_t disp_col, uint8_t disp_length, char *
 
 }
 
+void disp_thd()
+{
+    switch (harm_page) {
+        case 0:
+            disp_printf(0, 0, 16, "THD=%.1f%%", curr_thd*100);
+            disp_printf(1, 0, 16, "2nd:%.3fA", harmonic[2]);
+            disp_printf(2, 0, 16, "3th:%.3fA", harmonic[3]);
+            disp_printf(3, 0, 16, "4th:%.3fA", harmonic[4]);
+        break;
+        case 1:
+            disp_printf(0, 0, 16, "5th:%.3fA", harmonic[5]);
+            disp_printf(1, 0, 16, "6th:%.3fA", harmonic[6]);
+            disp_printf(2, 0, 16, "7th:%.3fA", harmonic[7]);
+            disp_printf(3, 0, 16, "8th:%.3fA", harmonic[8]);
+        break;
+        case 2:
+            disp_printf(0, 0, 16, "9th:%.3fA", harmonic[9]);
+            disp_printf(1, 0, 16, "10th:%.3fA", harmonic[10]);
+            disp_printf(2, 0, 16, "11th:%.3fA", harmonic[11]);
+            disp_printf(3, 0, 16, "12th:%.3fA", harmonic[12]);
+        break;
+        
+    }
+}
+
 void disp_proc()
 {
     uint8_t str[16]={0};
@@ -50,10 +76,7 @@ void disp_proc()
 
         break;
         case 1:
-            disp_printf(0, 0, 16, "THD=%.1f%%", curr_thd*100);
-            disp_printf(1, 0, 16, " ");
-            disp_printf(2, 0, 16, " ");
-            disp_printf(3, 0, 16, " ");
+            disp_thd();
             // snprintf(str, 16, "THD=%.3f       ", thd_disp);
             // disp_str_ascii(0, 0, str, 16);
         break;
